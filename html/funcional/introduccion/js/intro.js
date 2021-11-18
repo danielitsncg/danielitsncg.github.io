6e2edad8dd65971748c9f721ed4995344e8d8d5b
danielitsncg <71733906+danielitsncg@users.noreply.github.com>
2021-11-17 18:28:58 -0700

    fun

---
+++ html/funcional/introduccion/js/intro.js
@@ -0,0 +1,16 @@
+//Imperativa
+const hola = "pedro";
+const decirHola =()=>{
+    console.log(`Hi ${hola}`);
+}
+//decirHola();
+
+//const dHi=(name)=>`Hi ${name}`;
+
+//console.log(dHi("jose"));
+
+const sum = (a,b) => a + b
+const sum2 = (a,b) => a - b
+const suma = (a,b,ccc) =>ccc(a,b)
+
+console.log(suma(2,2,sum2));
